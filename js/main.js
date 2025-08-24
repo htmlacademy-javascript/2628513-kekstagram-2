b7d89193e39d174ebd4d32c38dce25cc76f2d140
Irina041708 <q-w-i-n-86@rambler.ru>
2025-08-24 13:41:14 +0400

    test1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 /* eslint-disable no-console */
-import {getPhotos} from './getPhotos.js';
+// import {getPhotos} from './getPhotos.js';
+import {template} from './photos.js';
 
-const PHOTO_COUNT = 25;
-const getAllPhotos = () => Array.from({length: PHOTO_COUNT}, getPhotos);
+console.log(template);
 
-// console.log(getPhotos());
-console.log(getAllPhotos());
+// const PHOTO_COUNT = 25;
+// const getAllPhotos = () => Array.from({length: PHOTO_COUNT}, getPhotos);
+
+// // console.log(getPhotos());
+// console.log(getAllPhotos());
---
+++ js/photos.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+
+const template = document.querySelector('#picture').content;
+
+// console.log(template);
+
+export {template};
